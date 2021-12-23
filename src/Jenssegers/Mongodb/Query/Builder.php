df89617529acddc6d940c8ecbe9b8820ab974771
CleytonBonamigo <cleytonbonamigo@gmail.com>
2021-12-22 23:13:38 -0300

    Melhoria para lógica de verificação de cache.

--- src/Jenssegers/Mongodb/Query/Builder.php
+++ src/Jenssegers/Mongodb/Query/Builder.php
@@ -409,8 +409,7 @@ class Builder extends BaseBuilder
             ];*/
             $cacheKey = $this->generateCacheKey();
             $tags = getEmpCacheTags(['select']);
-            if(/*config('app.env') != 'production' &&*/ \Cache::tags($tags)->has($cacheKey)){
-                $cacheObj = \Cache::tags($tags)->get($cacheKey);
+            if(/*config('app.env') != 'production' &&*/ $cacheObj = \Cache::tags($tags)->get($cacheKey)){
                 if($cacheObj){
                     return $cacheObj;
                 }
